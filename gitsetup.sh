ae7490b8a72f7abb83f6490da7d2b2e932df1533
Adam MacKinnon <mackinnon.adam@gmail.com>
2022-03-13 14:03:03 -0400

    added license and git setup

--- gitsetup.sh
+++ gitsetup.sh
@@ -3,10 +3,14 @@ echo "HELLO WORLD"
 
 $USERNAME=$1
 $REPONAME=$2
+$COMMIT="first commit"
 
 function createGit() {
+    git config --global init.defaultBranch main
     git init
+    git commit -m$COMMIT
+    git branch -M main
     git remote add origin https://github.com/$USERNAME/$REPONAME
-    git branch -m main
+    git push --set-upstream origin main
 }
 createGit
